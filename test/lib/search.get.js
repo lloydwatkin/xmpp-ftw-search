2bf2b82e397abafd74b1a3d9177a5e36eab0acb7
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-09-14 16:43:01 +0100

    Handle extensible search form fields

--- test/lib/search.get.js
+++ test/lib/search.get.js
@@ -127,5 +127,51 @@ describe('Get search form', function() {
             callback
         )
     })
+
+    it('Handles basic search form', function(done) {
+        xmpp.once('stanza', function() {
+            manager.makeCallback(helper.getStanza('form-x'))
+        })
+        var callback = function(error, data) {
+            should.not.exist(error)
+            data.instructions.should.equal('Search for characters')
+            should.not.exist(data.basic)
+            data.form.title.should.equal('User Directory Search')
+            data.form.instructions
+                .should.equal('Please provide the following information')
+            data.form.fields.length.should.equal(3)
+            var fields = data.form.fields
+            fields[0].should.eql({
+                var: 'first',
+                type: 'text-single',
+                required: false,
+                label: 'Given Name'
+            })
+            fields[1].should.eql({
+                var: 'last',
+                type: 'text-single',
+                required: false,
+                label: 'Family Name'                
+            })
+            fields[2].var.should.equal('x-gender')
+            fields[2].type.should.equal('list-single')
+            fields[2].required.should.be.false
+            fields[2].label.should.equal('Gender')
+            fields[2].options.length.should.equal(2)
+            fields[2].options[0].should.eql({
+                value: 'male', label: 'Male'  
+            })
+            fields[2].options[1].should.eql({
+                value: 'female', label: 'Female'
+            })
+            done()
+        }
+        var request = { to: 'characters.shakespeare.lit' }
+        socket.emit(
+            'xmpp.search.get',
+            request,
+            callback
+        )
+    })
     
 })
