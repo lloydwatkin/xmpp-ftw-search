e36f254bb82cfbc35482ff96ef342939d7cfb9bb
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-09-14 15:38:50 +0100

    Initial test holders and code in place

---
+++ test/lib/search.incoming.js
@@ -0,0 +1,34 @@
+var should = require('should')
+  , Search = require('../../lib/search')
+  , ltx    = require('ltx')
+  , helper = require('../helper')
+
+describe('Incoming messages', function() {
+
+    var search, socket, xmpp, manager
+
+    before(function() {
+        socket = new helper.Eventer()
+        xmpp = new helper.Eventer()
+        manager = {
+            socket: socket,
+            client: xmpp,
+            trackId: function(id, callback) {
+                this.callback = callback
+            },
+            makeCallback: function(error, data) {
+                this.callback(error, data)
+            }
+        }
+        search = new Search()
+        search.init(manager)
+    })
+
+    describe('Handles', function() {
+        
+        it('Returns false', function() {
+            avatar.handles(ltx.parse('<iq/>')).should.be.false
+        })
+    })
+    
+})
