9c20d359cd250b287c9b53779caa81979a02954c
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-09-14 16:33:36 +0100

    Add basic search form response

--- test/lib/search.get.js
+++ test/lib/search.get.js
@@ -1,6 +1,5 @@
 var should = require('should')
   , Search = require('../../lib/search')
-  , ltx    = require('ltx')
   , helper = require('../helper')
 
 describe('Get search form', function() {
@@ -22,6 +21,111 @@ describe('Get search form', function() {
         }
         search = new Search()
         search.init(manager)
+    })
+
+    it('Errors if no callback provided', function(done) {
+        xmpp.once('stanza', function() {
+            done('Unexpected outgoing stanza')
+        })
+        socket.once('xmpp.error.client', function(error) {
+            error.type.should.equal('modify')
+            error.condition.should.equal('client-error')
+            error.description.should.equal('Missing callback')
+            error.request.should.eql({})
+            xmpp.removeAllListeners('stanza')
+            done()
+        })
+        socket.emit('xmpp.search.get', {})
+    })
+
+    it('Errors if non-function callback provided', function(done) {
+        xmpp.once('stanza', function() {
+            done('Unexpected outgoing stanza')
+        })
+        socket.once('xmpp.error.client', function(error) {
+            error.type.should.equal('modify')
+            error.condition.should.equal('client-error')
+            error.description.should.equal('Missing callback')
+            error.request.should.eql({})
+            xmpp.removeAllListeners('stanza')
+            done()
+        })
+        socket.emit('xmpp.search.get', {}, true)
+    })
+
+    it('Errors if no content key provided', function(done) {
+        var request = {}
+        xmpp.once('stanza', function() {
+            done('Unexpected outgoing stanza')
+        })
+        var callback = function(error, success) {
+            should.not.exist(success)
+            error.type.should.equal('modify')
+            error.condition.should.equal('client-error')
+            error.description.should.equal('Missing \'to\' key')
+            error.request.should.eql(request)
+            xmpp.removeAllListeners('stanza')
+            done()
+        }
+        socket.emit(
+            'xmpp.search.get',
+            request,
+            callback
+        )
+    })
+    
+    it('Sends expected stanza', function(done) {
+        var request = { to: 'characters.shakespeare.lit' }
+        xmpp.once('stanza', function(stanza) {
+            stanza.is('iq').should.be.true
+            stanza.attrs.id.should.exist
+            stanza.attrs.to.should.equal(request.to)
+            stanza.attrs.type.should.equal('get')
+            stanza.getChild('query', Search.NS).should.exist
+            done()
+        })
+        socket.emit('xmpp.search.get', request, function() {})
+    })
+    
+    it('Handles error response', function(done) {
+        xmpp.once('stanza', function() {
+            manager.makeCallback(helper.getStanza('iq-error'))
+        })
+        var callback = function(error, success) {
+            should.not.exist(success)
+            error.should.eql({
+                type: 'cancel',
+                condition: 'error-condition'
+            })
+            done()
+        }
+        var request = { to: 'characters.shakespeare.lit' }
+        socket.emit(
+            'xmpp.search.get',
+            request,
+            callback
+        )
+    })
+    
+    it('Handles basic search form', function(done) {
+        xmpp.once('stanza', function() {
+            manager.makeCallback(helper.getStanza('form-basic'))
+        })
+        var callback = function(error, data) {
+            should.not.exist(error)
+            data.instructions.should.equal('Search for characters')
+            data.basic.length.should.equal(4)
+            data.basic.should.eql([
+                'first', 'last', 'nick', 'email'  
+            ])
+            done()
+        }
+        var request = { to: 'characters.shakespeare.lit' }
+        socket.emit(
+            'xmpp.search.get',
+            request,
+            callback
+        )
     })
     
 })
